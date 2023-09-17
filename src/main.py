ebce97e37d2a0a569908495ba9b4524fb890d1c6
Nirmal Baishnab <nirmal@iastate.edu>
2023-09-17 14:28:01 -0500

    edit: all basic functionality is added. 1. show a random position for some time 2. show empty board 3. take input from user as fen string 4. Shows if it is correct or not.
    To do:
    1. score counter
    2. create user input pipeline
    3. Clean some minor display bugs, like refresh the text message after replay
    4. add another button that shows the position again. called repeat.

--- src/main.py
+++ src/main.py
@@ -6,76 +6,72 @@ from io import BytesIO
 from PIL import Image, ImageTk
 import random
 import os
-from panels import *
+from panels import BoardDisplay
 
 import sys
 sys.path.append('../utils/')
 from fen_sampler import RandomFENSelector
 
 def replay():
-    display_random_position()
-    # After 10 seconds, display the empty board.
+    global last_shown_fen
+
+    # First, we set the last_shown_fen to a random position
+    last_shown_fen = display_random_position()
+    print(last_shown_fen)
+
+    # After 1 seconds, display the empty board.
     root.after(1000, lambda: fen_display(board_display, '8/8/8/8/8/8/8/8 w - - 0 1'))
+
+    # Clear and enable the FEN entry
+    fen_entry.delete(0, tk.END)
+    fen_entry.config(state=tk.NORMAL)
 
 def display_random_position():
     selector = RandomFENSelector("/home/nirmal/Research/others/blindfold_chess/data/pgns")
     random_position = selector.get_random_position()
     fen_display(board_display, random_position)
+    return random_position  # return the random_position so we can update the last_shown_fen
 
 def main():
-    global selector, root, board_display  # Declare these as globals so that they can be accessed within the reset_display function
-
+    global selector, root, board_display, panels, fen_entry, result_label
+    
     root = tk.Tk()
     board_display = BoardDisplay(root)
     
     # Reset button
     reset_button = tk.Button(root, text="replay", command=replay)
-    reset_button.pack()  # Adjust this to place the button wherever you want
+    reset_button.pack()
+    
+    # FEN Entry widget
+    fen_entry = tk.Entry(root)
+    fen_entry.pack()
+    fen_entry.config(state=tk.DISABLED)  # Disabled until the board is cleared
+
+    # Button to validate entered FEN
+    check_button = tk.Button(root, text="Check FEN", command=validate_fen)
+    check_button.pack()
+
+    # Label to display result of FEN validation
+    result_label = tk.Label(root, text="")
+    result_label.pack()
 
     # Initially start the process once
     replay()
-    
+
     root.mainloop()
-
-
-
-
-
-    # refresh_callback = lambda: fen_display(board_display)
-    # refresh_button = RefreshButton(root, refresh_callback)
-    # control_panel.add_control(refresh_button)
-
-    n_seconds = 5  # for example, to refresh every 5 seconds
-    #auto_refresh(root, refresh_callback, n_seconds * 1000)  # Convert to milliseconds
-
-
-
-    # directory = "/home/nirmal/Research/others/blindfold_chess/data/pgns/"
-    # random_pgn_file = get_random_pgn_file(directory)
-    # fen_string = get_random_fen_from_pgn(os.path.join(directory, random_pgn_file))
-
-    # root = tk.Tk()
-    # root.title("Chess Board Toggle")
-    # display = ChessDisplay(root, fen_string, wait_time=2)
-    # root.mainloop()
-
-    
-    # display the position on the board for 10 seconds
-
-    # display the empty board
-
-    # ask the user to enter the position as FEN string
-
-    # check if the entered FEN string is correct
-
-    # if correct say correct or else say incorrect
-
-    # get ready to repeat the process if the replay button is pressed
 
 
 def fen_display(board_display, fen_string='8/8/8/8/8/8/8/8 w - - 0 1'):
     board_display.update_display(fen_string)
 
+def validate_fen():
+    entered_fen = fen_entry.get().strip()  # Removing any extra spaces
+    piece_placement_from_last_shown_fen = last_shown_fen.split(' ')[0]  # Take only the piece placement part
+
+    if entered_fen == last_shown_fen or entered_fen == piece_placement_from_last_shown_fen:
+        result_label.config(text="Correct!")
+    else:
+        result_label.config(text="Incorrect! Try again.")
 
 if __name__ == "__main__":
     main()
